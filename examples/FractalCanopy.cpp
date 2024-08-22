faca6efd6fd6277d7c94c4203bd02ec7010b81af
assyyn <106226278+Assyyn@users.noreply.github.com>
2024-08-22 12:41:49 +0545

    bugfix: add max_depth to fractal canopy generation.
    - track interesting renders.
    - add sanitizers to compile/link options

--- examples/FractalCanopy.cpp
+++ examples/FractalCanopy.cpp
@@ -93,38 +93,46 @@ struct BranchChildren
     Line2D second;
 };
 
-BranchChildren
-gen_children(const Line2D &root, math::Degree angle, double scale_factor)
+BranchChildren gen_children(const Line2D      &parent,
+                            const math::Degree angle,
+                            const double       scale_factor)
 {
     auto extend_line_endpoint = [](const Line2D &line,
-                                   double        length) -> Point2D {
+                                   double        new_length) -> Point2D {
         const auto len = line.length();
         const auto dx  = line.end.x - line.start.x;
         const auto dy  = line.end.y - line.start.y;
 
+        assert(new_length > 0);
+
         return {
-            narrow_cast<int>(line.end.x + dx / len * length),
-            narrow_cast<int>(line.end.y + dy / len * length),
+            narrow_cast<int>(line.end.x + dx / len * new_length),
+            narrow_cast<int>(line.end.y + dy / len * new_length),
         };
     };
 
     const auto initial_endpoint =
-        extend_line_endpoint(root, root.length() * scale_factor);
+        extend_line_endpoint(parent, parent.length() * scale_factor);
 
     return BranchChildren {
-        .first  = {root.end,rotate_copy(initial_endpoint,angle, root.end)         },
-        .second = {root.end,
-                   rotate_copy(initial_endpoint,
-                   {360 - angle.angle},
-                   root.end)},
+        .first = {parent.end,rotate_copy(initial_endpoint,angle,  parent.end)        },
+        .second = {parent.end,
+                  rotate_copy(initial_endpoint,
+                  {360 - angle.angle},
+                  parent.end)},
     };
 }
+static_assert(math::pow(2, 8) == 256);
 
-Canopy gen_canopy(Line2D       root,
-                  math::Degree angle,
-                  double       scale_factor = 3.0 / 4,
-                  double       min_length   = 1)
+Canopy gen_canopy(Line2D             root,
+                  const math::Degree angle,
+                  const double       scale_factor,
+                  std::int64_t       max_depth  = 0xfff,
+                  const double       min_length = 1)
 {
+    assert(scale_factor < 1);
+    assert(min_length >= 1);
+
     // start with a parent.
     // push it to the vector.
     // generate the child branches.
@@ -132,33 +140,32 @@ Canopy gen_canopy(Line2D       root,
     // keep going while the branches are larger than min_length.
 
     Canopy canopy;
+    struct QueueElement
     {
-        auto iterations = 0;
-        auto len        = root.length();
-        while (len >= min_length) {
-            len *= scale_factor;
-            iterations++;
-        }
+        Line2D line;
+        int    depth = 0;
+    };
+    std::queue<QueueElement> queue;
+    queue.push({root});
 
-        canopy.reserve(iterations);
-    }
-    std::queue<Line2D> queue;
-    queue.push(root);
-
+    // TODO: find a more elegant way
     while (!queue.empty()) {
         const auto current = queue.front();
         queue.pop();
 
-        canopy.push_back(current);
+        if (current.line.length() < min_length) {
+            continue;    // go through every line in queue
+        }
+        canopy.push_back(current.line);
 
-        if (current.length() < min_length) {    // abort when too small
-            break;
+        if (current.depth > max_depth) {
+            continue;
         }
 
         const auto [child_A, child_B] =
-            gen_children(current, angle, scale_factor);
-        queue.push(child_A);
-        queue.push(child_B);
+            gen_children(current.line, angle, scale_factor);
+        queue.push({child_A, current.depth + 1});
+        queue.push({child_B, current.depth + 1});
     }
 
     return canopy;
@@ -168,17 +175,26 @@ Canopy gen_canopy(Line2D       root,
 
 int main()
 {
-    constexpr int    image_width  = 1'920;
-    constexpr int    image_height = 1'080;
+    constexpr math::Degree child_rotation {60};
+    constexpr auto         scale_factor = 0.5;
+    constexpr auto         min_length   = 1;
+    constexpr auto         max_depth    = 10;
 
-    Image            image {image_width, image_height};
+    constexpr int          image_width  = 1'920;
+    constexpr int          image_height = 1'080;
 
-    constexpr Line2D base {
-        .start = {image_width / 2,          image_height},
-        .end   = {image_width / 2, image_height * 8 / 10},
+    Image                  image {image_width, image_height};
+
+    constexpr Line2D       base {
+              .start = {image_width / 2,     image_height},
+              .end   = {image_width / 2, image_height / 2},
     };
 
-    for (const auto &line : gen_canopy(base, {20})) {
+    for (const auto &line : gen_canopy(base,
+                                       child_rotation,
+                                       scale_factor,
+                                       max_depth,
+                                       min_length)) {
         draw_line(image, line);
     }
 
